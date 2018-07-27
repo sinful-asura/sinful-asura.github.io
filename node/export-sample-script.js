ed645d98d18a4d4a4c4efef17b05c829c4ba801e
KileX <it.kristijan.jovic@gmail.com>
2018-07-27 23:26:16 +0200

    node directory

---
+++ node/export-sample-script.js
@@ -0,0 +1,5 @@
+const largeNumber = 356;
+
+module.exports ={ 
+	largeNumber: largeNumber
+};
---
+++ node/useful-node-data.js
@@ -0,0 +1,5 @@
+const {largeNumber} = require('./export-sample-script.js');
+const fs = require('fs'); //File System
+const http = require('http'); //Hyper Text Transfer Protocol
+const https = require('https'); //Hyper Text Transfer Protocol Secure
+const dns = require('dns'); // Domain Name Server
